6e27521ed25554e944bd5dbff9c7ecae332e5333
Jose Carlos <80229893+josecaog26@users.noreply.github.com>
2022-06-16 18:36:47 +0100

    Trabajo final

--- Assets/Patrol.cs
+++ Assets/Patrol.cs
@@ -9,8 +9,13 @@ public class Patrol : MonoBehaviour
 {
     NavMeshAgent agent;
 
-    float random_destination_radius = 10.0f;
+   // float random_destination_radius = 10.0f;
+    
     Vector3 InitialPosition;
+
+    public Transform[] waypoints;
+    private int currentWaypointIndex = 0;
+    //private float speed = 4f;
     
 
     void Start()
@@ -18,14 +23,55 @@ public class Patrol : MonoBehaviour
         agent = GetComponent<NavMeshAgent>();
         InitialPosition = agent.transform.position;
         
+        
     }
-    
+
+    void Update()
+    {
+        Transform wp = waypoints[currentWaypointIndex];
+        if (Vector3.Distance(agent.transform.position, wp.position) < 2f)
+        {
+            currentWaypointIndex = (currentWaypointIndex + 1) % waypoints.Length;
+        }
+    }
 
     [Task]
-    bool SetDestination_Random()
+    bool MoveToWaypoints()
     {
-        var distancia = this.transform.position + (Random.insideUnitSphere * random_destination_radius);
+        Transform wp = waypoints[currentWaypointIndex];
+
+        agent.SetDestination(wp.position);
+        return true;
+
+    }
+
+    
+/*
+    [Task]
+    void MoveToWaypoints()
+    {
+        Transform wp = waypoints[currentWaypointIndex];
+        
+        transform.position = Vector3.MoveTowards(transform.position, wp.position, speed * Time.deltaTime);
+        agent.SetDestination(transform.position);
+    }
+*/
+    
+    float tiempo = 5.0f;
+    
+    [Task]
+    bool Contador()
+    {
+        /*var distancia = this.transform.position + (Random.insideUnitSphere * random_destination_radius);
         agent.SetDestination(distancia);
+        return true;*/
+        
+        
+        tiempo -= Time.deltaTime;
+        if (tiempo <= 0){
+            return false;
+            
+        }
         return true;
     }
 
@@ -34,6 +80,38 @@ public class Patrol : MonoBehaviour
     
     bool awareOfPlayer = false;
 
+    [Task]
+    void GoLastSeenPosition()
+    {
+        /*var player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        enemyLastSeenPosition = player.transform.position;
+        agent.SetDestination(enemyLastSeenPosition);
+*/
+        var player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        
+        RaycastHit hit;
+
+        Vector3 direction = player.transform.position - transform.position;
+
+        if (Physics.Raycast(transform.position, direction, out hit))
+        {
+            if (hit.collider.CompareTag("Player"))
+            {
+                enemyLastSeenPosition = player.transform.position;
+                awareOfPlayer = true;
+                Task.current.Fail();
+            }
+            
+        }
+        
+        
+ /*       if (Vector3.Distance(agent.transform.position, enemyLastSeenPosition) < 2f)
+        {
+        
+        }
+        */
+        
+    }   
 
     [Task]
     bool CanSeePlayer()
@@ -51,17 +129,20 @@ public class Patrol : MonoBehaviour
             {
                 enemyLastSeenPosition = player.transform.position;
                 awareOfPlayer = true;
+                tiempo = 5.0f;
                 return true;
             }
             agent.SetDestination(hit.point);
             
         }
         return false;
+        
     }
 
     [Task]
     void ChasePlayer()
     {
+
         agent.SetDestination(enemyLastSeenPosition);
         Task.current.Succeed();
 
@@ -70,7 +151,9 @@ public class Patrol : MonoBehaviour
     [Task]
     bool AwareOfPlayer()
     {
+
         return awareOfPlayer;
+        
     }
     
     [Task]
